446bee002ccc0fa1643dbec0a58072c8d9223ea3
leitongchen <leitongchen93@gmail.com>
2021-04-23 17:04:35 +0200

    somma peso delle zucchine

--- zucchine.js
+++ zucchine.js
@@ -1,5 +1,47 @@
+function printThemAll(zucchini, gr, cm) {
+
+    var singleZucchini = [];
+
+    for (var key in zucchini) {
+
+        if (key === gr) {
+
+            singleZucchini.push(key + ": " + zucchini[key] + "gr");
+
+        } else if (key === cm) {
+
+            singleZucchini.push(key + ": " + zucchini[key] + "cm");
+        } else {
+
+            singleZucchini.push(key + ": " + zucchini[key]);
+        }
+
+    }
+    return singleZucchini;
+    //return element + zucchini.variety + " - " + zucchini.weight + "gr - " + zucchini.length + "cm";
+}
+
+
+// funzione: genera in modo automatico la varietà da assegnare alla zucchina
+function setVariety(array) {
+
+    return array[randomNumGen(1, array.length - 1)]
+}
+
+
+function randomNumGen(min, max) {
+    var minMax = max - min;
+
+    var randomNum = Math.floor(Math.random() * (minMax + 1)) + min;
+
+    return randomNum;
+}
+
+
 (function () {
 
+    var yourZucchini = document.getElementById('your-zucchini');
+    var zucchiniWeight = document.getElementById("your-zucchini-weight");
     /* 
     [Snack 1]
     Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà,
@@ -12,6 +54,7 @@
 
     var zucchiniVariety = ['black beauty', 'caserta', 'golden egg', 'cocozella', 'dunja', 'round de nice', 'zephyr', 'gadzukes'];
 
+    // array in cui verranno inserite le 10 zucchine
     var allZucchini = [];
 
     // variety, weight e length vengono assegnati in automatico con un ciclo 
@@ -22,35 +65,38 @@
             variety: setVariety(zucchiniVariety),
             weight: randomNumGen(50, 450),
             length: randomNumGen(10, 23),
-        }       
+        }
 
         allZucchini.push(newZucchini);
     }
 
-    console.log(allZucchini);
 
+    // STAMPA LE 10 ZUCCHINE IN HTML
+    // SOMMARE IL PESO DI TUTTE LE ZUCCHINE
 
+    var allZucchiniWeight = 0;
+    for (var i = 0; i < allZucchini.length; i++) {
 
-    
+        var oneZucchini = allZucchini[i];
 
-    // funzione: genera in modo automatico la varietà da assegnare alla zucchina
-    function setVariety(array) {
+        for (var key in oneZucchini) {
 
-       return array[randomNumGen(1, array.length-1)]
+            if (key === "weight") {
+
+                allZucchiniWeight += parseInt(oneZucchini[key]);
+            }
+        }
+        yourZucchini.innerHTML += "<li><strong>Zucchina " + (i + 1) + ": </strong><br> " + printThemAll(oneZucchini, 'weight', 'length').join(" * ") + "</li>";
     }
 
-    //console.log(setVariety(zucchiniVariety))
+    zucchiniWeight.innerHTML += "<strong>" + allZucchiniWeight + "gr </strong>";
+
+ 
 
 
-    //console.log(randomNumGen(10, 23));
 
-    function randomNumGen(min, max) {
-        var minMax = max - min;
 
-        var randomNum = Math.floor(Math.random() * (minMax + 1)) + min;
 
-        return randomNum;
-    }
 
 
 
